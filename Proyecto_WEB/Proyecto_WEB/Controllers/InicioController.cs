8b5425266ac671822997cb0560cb916aba57b3b1
mari <maripazchg@hotmail.com>
2024-06-05 21:17:46 -0600

    cambios a avance

--- Proyecto_WEB/Proyecto_WEB/Controllers/InicioController.cs
+++ Proyecto_WEB/Proyecto_WEB/Controllers/InicioController.cs
@@ -4,16 +4,16 @@ using System.Diagnostics;
 
 namespace Proyecto_WEB.Controllers
 {
-    public class HomeController : Controller
+    public class InicioController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<InicioController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public InicioController(ILogger<InicioController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Inicio()
         {
             return View();
         }
